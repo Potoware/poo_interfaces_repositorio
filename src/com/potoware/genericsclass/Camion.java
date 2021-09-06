efbb6c90dadd49340e34dee5eb6c3f7fc70032fd
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-06 11:45:40 -0500

    metodos genericos, en clases genericas, creacion de clases

---
+++ src/com/potoware/genericsclass/Camion.java
@@ -0,0 +1,29 @@
+package com.potoware.genericsclass;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Camion implements Iterable{
+
+    private List objetos;
+    private int max;
+
+    public Camion(int max) {
+        this.max = max;
+        this.objetos = new ArrayList();
+    }
+
+    public void add(Object object){
+        if(this.objetos.size() >= max){
+        this.objetos.add(object);
+        }else{
+            throw new RuntimeException("No hay mas espacio");
+        }
+    }
+
+    @Override
+    public Iterator iterator() {
+        return objetos.iterator();
+    }
+}
