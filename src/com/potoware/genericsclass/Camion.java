8ee276c2389fa4a814b8db272520af9e930e0f4f
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-06 12:07:41 -0500

    metodos genericos, en clases genericas, creacion de clases

--- src/com/potoware/genericsclass/Camion.java
+++ src/com/potoware/genericsclass/Camion.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Camion implements Iterable{
+public class Camion <T>implements Iterable<T>{
 
-    private List objetos;
+    private List<T> objetos;
     private int max;
 
     public Camion(int max) {
         this.max = max;
-        this.objetos = new ArrayList();
+        this.objetos = new ArrayList<>();
     }
 
-    public void add(Object object){
+    public void add(T object){
         if(this.objetos.size() <= max){
         this.objetos.add(object);
         }else{
@@ -23,7 +23,7 @@ public class Camion implements Iterable{
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return objetos.iterator();
     }
 }
--- src/com/potoware/genericsclass/EjemploGenericos.java
+++ src/com/potoware/genericsclass/EjemploGenericos.java
@@ -6,37 +6,42 @@ public class EjemploGenericos {
     public static void main(String[] args) {
 
         System.out.println("======== Caballos");
-        Camion camionCaballos = new Camion(5);
+        Camion<Animal> camionCaballos = new Camion<>(5);
         camionCaballos.add(new Animal("Pererino","Caballo"));
         camionCaballos.add(new Animal("Grillo", "Caballo"));
         camionCaballos.add(new Animal("Mateus", "Caballo"));
         camionCaballos.add(new Animal("Topocalma", "Caballo"));
         camionCaballos.add(new Animal("Fonsopior", "Caballo"));
 
-        for(Object o: camionCaballos){
-            Animal a = (Animal) o;
-            System.out.println(((Animal) o).getNombre()+ " "+((Animal) o).getTipo());
-                    }
+        imprimirCamion(camionCaballos);
+
 
         System.out.println("======== MAQUINARIA");
-        Camion camionMaquinas = new Camion(3);
+        Camion<Maquinaria> camionMaquinas = new Camion<>(3);
         camionMaquinas.add(new Maquinaria("Bulldozer"));
         camionMaquinas.add(new Maquinaria("Grua"));
         camionMaquinas.add(new Maquinaria("Escabadora"));
+        imprimirCamion(camionMaquinas);
 
-        for(Object o : camionMaquinas){
-            Maquinaria a = (Maquinaria) o;
-            System.out.println(((Maquinaria) o).getTipo());
-        }
         System.out.println("======== Automovil");
-        Camion camionAutos = new Camion(3);
+        Camion<Automovil> camionAutos = new Camion<>(3);
         camionAutos.add(new Automovil("Toyoya"));
         camionAutos.add(new Automovil("Mercedes Benz"));
         camionAutos.add(new Automovil("BMW"));
+        imprimirCamion(camionAutos);
 
-        for(Object o: camionAutos){
-            Automovil a = (Automovil) o;
-            System.out.println(((Automovil) o).getMarca());
+    }
+    public static <T> void imprimirCamion(Camion<T> camion){
+        for(T a: camion){
+            if (a instanceof Animal) {
+                System.out.println(((Animal)a).getNombre() + " " + ((Animal)a).getTipo());
+            }
+            else if(a instanceof Maquinaria){
+                System.out.println(((Maquinaria)a).getTipo());
+            }
+            else if(a instanceof Automovil){
+                System.out.println(((Automovil)a).getMarca());
+            }
         }
     }
 }
